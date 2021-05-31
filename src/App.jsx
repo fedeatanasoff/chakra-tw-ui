5a35f7df2751acbf34b6a7d703462cdf4eb59458
fedeatanasoff <fede.atanasoff@gmail.com>
2021-05-31 01:10:01 -0300

    init layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Stack, Text } from '@chakra-ui/layout';
 import React from 'react';
-import { Text } from '@chakra-ui/react';
 import Layout from './Layout';
 
 function App() {
-  return <Layout>hello friend</Layout>;
+  return (
+    <Layout>
+      <Stack direction="row">
+        <Stack>
+          <Text>logo</Text>
+        </Stack>
+      </Stack>
+    </Layout>
+  );
 }
 
 export default App;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,12 @@
+import { Container } from '@chakra-ui/layout';
 import React from 'react';
 
 const Layout = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <Container bg="red.300" maxW="container.md">
+      {children}
+    </Container>
+  );
 };
 
 export default Layout;
